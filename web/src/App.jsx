530c81c669828c1dcff5e856cc24e2a0a42b680f
JP-miner <joaopedroriosleal@gmail.com>
2024-11-21 20:48:28 -0300

    proxima alteracao

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -11,7 +11,7 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 import 'primeicons/primeicons.css';
 
-// COMENTARIO
+// qualquer coisa
 const App = ({ children }) => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
